0a713dffb8d794c992fd6e837e724e73105343ae
RAZEX0LOL <99183060+RAZEX0LOL@users.noreply.github.com>
2024-02-28 20:54:54 +0400

    Clear project directory and add components
    Clear project in public and src, and add directory "components" with added components Header and Footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <header>
+            <div>
+                <span className="logo">Phone Sell</span>
+            </div>
+            <div className="presentation"></div>
+        </header>
+    );
+}
