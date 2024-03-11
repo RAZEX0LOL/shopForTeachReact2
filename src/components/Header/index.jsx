f8722f92194c44aa8db4ad7b61306cae864f2e10
RAZEX0LOL <khattayev00@mail.ru>
2024-03-11 20:59:12 +0400

    Add images,objects,new components and styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,12 @@ export default function Header(){
     return(
         <header>
             <div>
-                <span className={styles.logo}>Phone Sell</span>
+                <span className={styles.logo}>Hi-Tech Hub</span>
+                <ul className={styles.nav}>
+                    <li>О нас</li>
+                    <li>Контакты</li>
+                    <li>Личный кабинет</li>
+                </ul>
             </div>
             <div className={styles.presentation}></div>
         </header>
