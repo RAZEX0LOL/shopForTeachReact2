1284f2a5b3f7900f4bcb4676c52929f99ea6f38f
RAZEX0LOL <khattayev00@mail.ru>
2024-03-29 11:22:55 +0400

    modify styles in Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-// import styles from "./Footer.module.scss";
+import styles from "./Footer.module.scss";
 
 const Footer:React.FC=()=>{
     return(
-        <footer>
+        <footer style={styles.footer}>
             Все права защищены &copy;
         </footer>
     );
